42302d25ab9667cc76624290860488e1b840f1cc
MatusTuriak <turiakmatus@gmail.com>
2025-05-11 21:21:51 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bullettemp = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullettemp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(330, 720);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(73, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bullettemp
+            // 
+            this.bullettemp.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.bullettemp.Location = new System.Drawing.Point(364, 699);
+            this.bullettemp.Name = "bullettemp";
+            this.bullettemp.Size = new System.Drawing.Size(5, 15);
+            this.bullettemp.TabIndex = 1;
+            this.bullettemp.TabStop = false;
+            this.bullettemp.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 803);
+            this.Controls.Add(this.bullettemp);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullettemp)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox bullettemp;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+            this.MouseDown += Form1_MouseDown;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                PictureBox bullet = new PictureBox();
+                bullet.Size = bullettemp.Size;
+                bullet.BackColor = bullettemp.BackColor;
+ 
+                bullet.Location = new Point(
+                    pictureBox1.Left + pictureBox1.Width / 2 - bullet.Width / 2,
+                    pictureBox1.Top - bullet.Height
+                );
+
+                this.Controls.Add(bullet); 
+                bullet.BringToFront();
+
+                Timer bulletTimer = new Timer();
+                bulletTimer.Interval = 10;
+                bulletTimer.Tick += (s, ev) =>
+                {
+                    bullet.Top -= 10;
+                    if (bullet.Top < 0)
+                    {
+                        bulletTimer.Stop();
+                        this.Controls.Remove(bullet);
+                        bullet.Dispose();
+                    }
+                };
+                bulletTimer.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            int y = (ClientSize.Height - pictureBox1.Height);
+            int x = (ClientSize.Width - pictureBox1.Width) / 2;
+            pictureBox1.Location = new Point(x, y);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int moveAmount = 10;
+            int newX = pictureBox1.Left;
+
+            if (e.KeyCode == Keys.Right)
+            {
+                newX += moveAmount;
+                if (newX + pictureBox1.Width > ClientSize.Width)
+                    newX = ClientSize.Width - pictureBox1.Width;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                newX -= moveAmount;
+                if (newX < 0)
+                    newX = 0;
+            }
+
+            pictureBox1.Location = new Point(newX, pictureBox1.Top);
+
+        }
+    }
+}
